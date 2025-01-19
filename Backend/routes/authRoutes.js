2f528818f93ffa128de5980de7577d8b660c4fae
VAS-18 <uday.vas18@gmail.com>
2025-01-19 21:17:55 +0530

    Routes Updated

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-
+import { profileUpload } from "../middleware/uploadMiddleware.js";
 import { register, login } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.route("/register").post(register);
+router.route("/register").post(profileUpload ,register);
 
 router.route("/login").post(login);
 
