b6ca26ae47da6799dc92645dc12dafe892126f71
VAS-18 <uday.vas18@gmail.com>
2025-04-23 15:15:08 +0530

    Implement code changes to enhance functionality and improve performance

--- Frontend/src/pages/NotFound.jsx
+++ Frontend/src/pages/NotFound.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function NotFound() {
-  return <h1>404 - Not Found</h1>;
+  return (
+    <div className='flex flex-col items-center justify-center h-screen bg-[#f4b049]'>
+      <img src="/404Image.png" alt="404 Not Found" height={200} width={200}/>
+      <span className='text-3xl font-bold'>404 :(</span>
+      <p className='font-semibold'>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
 }
